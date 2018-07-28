2f9c7dc3a065db201e5e1cf2e33b76ace849aafd
Unknown <nathan-r@hotmail.com.au>
2018-07-28 16:56:01 +1000

    Created encryption class and added Rijndael encryption

---
+++ Crypt/Encryption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypt
+{
+    public static class Encryption
+    {
+        public static byte[] RijndaelEncrypt(byte[] input, byte[] key)
+        {
+            RijndaelManaged rijAlg = new RijndaelManaged
+            {
+                Key = key,
+                Mode = CipherMode.ECB
+            };
+            ICryptoTransform encrypter = rijAlg.CreateEncryptor();
+
+            return encrypter.TransformFinalBlock(input, 0, input.Length);
+        }
+    }
+}
